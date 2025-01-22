eda07a7296c08127ecad6d55087a24116d603e4e
layumih <ayumi.cod06@gmail.com>
2025-01-22 12:53:01 -0300

    implementando verificações do next-auth

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -2,7 +2,7 @@
 
 import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { AuthContext } from "@/context/authContext";
+import { AuthContext } from "@/context/authContext"; 
 import axios from "axios";
 import { FaSpinner } from "react-icons/fa";
 import { toast } from "react-toastify";

e5c66554b063d4210d0d4b408deec2e89f11ba43
layumih <ayumi.cod06@gmail.com>
2025-02-27 20:13:10 -0300

    tentando redirecionar para adminDashboard na vps

--- frontend/src/app/adminDashboard/page.js
+++ frontend/src/app/adminDashboard/page.js
@@ -2,7 +2,7 @@ import { checkSession } from "@/utils/checkSession";
 import { redirect } from "next/navigation";
 import DashboardContent from "@/components/adminDashboard/DashboardContent";
 
-export default async function DashboardPage() {
+export default async function AdminDashboard() {
   const session = await checkSession('/dashboard');
 
   if (!session) {

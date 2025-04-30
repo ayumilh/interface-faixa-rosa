5cfa8ff08259b88acc2d84f04d98c8fdd14b922c
layumih <ayumi.cod06@gmail.com>
2025-04-30 17:12:09 -0300

    commit

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -28,7 +28,7 @@ export default function HomePage() {
             alt="Ícone oficial Faixa Rosa"
             width={50}
             height={50}
-            className="animate-pulse w-auto h-auto"
+            className="animate-pulse w-12 h-12"
           />
         </div>
       )}
